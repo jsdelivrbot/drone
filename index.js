eedb286dc04701b79aebfeaaa23a2b5b88b85a41
Denis Howe <denis.howe@gmail.com>
2018-07-02 14:14:55 +0100

    Test push to multiple repos

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const PORT = process.env.PORT || 5000
 
-const handleRequest = (req, res) => res.send('Hello World!')
+const handleRequest = (req, res) => res.send('Hello, World!')
 
 express()
   .get('/', handleRequest)
