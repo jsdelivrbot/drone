ce4374f76a296c4c4f8227b3cb5e9bd36601427a
Denis Howe <denis.howe@gmail.com>
2018-07-02 18:49:44 +0100

    Test remote memcache

--- cache.js
+++ cache.js
@@ -1,28 +1,10 @@
 const memjs = require('memjs');
 
-// Connection details with defaults for local testing
+// Export client connected to memcached
 
-console.log("Env", process.env)
-const server = process.env.MEMCACHEDCLOUD_SERVERS
-	  ||
-	  'memcached-19275.c12.us-east-1-4.ec2.cloud.redislabs.com:19275'
+const client = memjs.Client.create(process.env.MEMCACHEDCLOUD_SERVERS, {
+	username: process.env.MEMCACHEDCLOUD_USERNAME,
+	password: process.env.MEMCACHEDCLOUD_PASSWORD
+})
 
-const login = {
-	username: process.env.MEMCACHEDCLOUD_USERNAME || 'memcached-app101467974',
-	password: process.env.MEMCACHEDCLOUD_PASSWORD || 't0cZC4eMZIcBrGaH7yddQpX65TxMcIVl'
-	}
-
-// Export client else false if can't connect
-
-let client, connected;
-
-try {
-	client = memjs.Client.create(server, login)
-	connected = client.servers[0].connected
-} catch (err) { }
-
-if (! connected) {
-	console.log("Can't connect", client);
-}
-
-module.exports = connected && client
+module.exports = client
--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 const express = require('express')
 const PORT = process.env.PORT || 5000
 
-const cache = require('./cache.js');
+const cacheClient = require('./cache.js');
 
 const handleRequest = (req, res) => {
-	if (! cache) {
-		res.send('Failed to connect to cache server');
-		return
-	}
-	cache.set('foo', 'bar')
-	cache.get('foo', function (err, value, key) {
+	cacheClient.set('foo', 'bar', {expires: 600})
+		.catch(e => console.log('set failed', e))
+	cacheClient.get('foo', function (err, value, key) {
 		const valueFromCache = value ? value.toString() : 'No value'
 		res.send(`From cache: ${ valueFromCache }`)
 	})
