bd152eb1c1eca0a66893fd0b127a29381c0c7432
Denis Howe <denis.howe@gmail.com>
2018-07-02 16:58:45 +0100

    Add memcache

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const express = require('express')
 const PORT = process.env.PORT || 5000
 
-const handleRequest = (req, res) => res.send('Hello, World!')
+const cache = require('./cache.js');
+
+const handleRequest = (req, res) => {
+	if (! cache) {
+		res.send('Failed to connect to cache server');
+		return
+	}
+	cache.set('foo', 'bar')
+	cache.get('foo', function (err, value, key) {
+		const valueFromCache = value ? value.toString() : 'No value'
+		res.send(`From cache: ${ valueFromCache }`)
+	})
+}
 
 express()
   .get('/', handleRequest)
