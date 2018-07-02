1e84f827fcaf28c5850ed23a59c8158685cac143
Denis Howe <denis.howe@gmail.com>
2018-07-02 21:03:46 +0100

    fetch and cache

--- index.js
+++ index.js
@@ -1,23 +1,18 @@
 const express = require('express')
 const PORT = process.env.PORT || 5000
 
-const cacheClient = require('./cache.js');
-const getDroneById = require('./fetch.js');
+const getCachedDroneById = require('./fetchCached')
 
 const handleRequest = (req, res) => {
-	// cacheClient.set('foo', 'bar', {expires: 600})
-	// 	.catch(e => console.log('set failed', e))
-	// cacheClient.get('foo', function (err, value, key) {
-	// 	const valueFromCache = value ? value.toString() : 'No value'
-	// 	res.send(`From cache: ${ valueFromCache }`)
-	// })
-
-	getDroneById(2,
-		error => { res.send(error) },
-		droneJsonString => { res.send(droneJsonString) }
-	)
+	getCachedDroneById(2,
+		error => {
+			res.send(error)
+		},
+		droneJsonString => {
+			res.send(droneJsonString)
+		})
 }
 
 express()
   .get('/', handleRequest)
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .listen(PORT, () => console.log('Listening on ' + PORT))
