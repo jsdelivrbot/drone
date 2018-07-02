021b5ba647557f39da994d53cd9d3544b565d85a
Denis Howe <denis.howe@gmail.com>
2018-07-02 20:36:59 +0100

    fetch

--- fetch.js
+++ fetch.js
@@ -4,10 +4,14 @@ const request = require('superagent')
 
 const apiByIdUrl = 'https://bobs-epic-drone-shack-inc.herokuapp.com/api/v0/drone/';
 
-const getDroneById = droneId => {
-	request.get(apiByIdUrl . droneId)
+const getDroneById = (droneId, fail, done) => {
+	request.get(apiByIdUrl + droneId)
+		.timeout({
+			response: 3000,  // 3000 ms for the server to start sending
+			deadline: 4000,  // ms to finish loading
+		})
 		.end((err, res) => {
-			console.log("res", res, "err", err)
+			return err ? fail('Fetch failed: ' + err) : done(res.text)
 		})
 }
 
--- index.js
+++ index.js
@@ -2,14 +2,20 @@ const express = require('express')
 const PORT = process.env.PORT || 5000
 
 const cacheClient = require('./cache.js');
+const getDroneById = require('./fetch.js');
 
 const handleRequest = (req, res) => {
-	cacheClient.set('foo', 'bar', {expires: 600})
-		.catch(e => console.log('set failed', e))
-	cacheClient.get('foo', function (err, value, key) {
-		const valueFromCache = value ? value.toString() : 'No value'
-		res.send(`From cache: ${ valueFromCache }`)
-	})
+	// cacheClient.set('foo', 'bar', {expires: 600})
+	// 	.catch(e => console.log('set failed', e))
+	// cacheClient.get('foo', function (err, value, key) {
+	// 	const valueFromCache = value ? value.toString() : 'No value'
+	// 	res.send(`From cache: ${ valueFromCache }`)
+	// })
+
+	getDroneById(2,
+		error => { res.send(error) },
+		droneJsonString => { res.send(droneJsonString) }
+	)
 }
 
 express()
