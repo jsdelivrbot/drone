feeef56f063cccecb0f39f3eca493c19296e5474
Denis Howe <denis.howe@gmail.com>
2018-07-02 21:47:44 +0100

    Retry origin fetch if cache empty

--- fetchCached.js
+++ fetchCached.js
@@ -4,29 +4,33 @@
 const getDroneById = require('./fetch.js');
 const cacheClient = require('./cache.js');
 
+const maxTries = 3;
+
 const getCachedDroneById = (droneId, fail, done) => {
-	// Try to get drone from origin
-	getDroneById(droneId,
-		originError => {
-			// Origin failed
-			console.log('Origin error', originError)
-			// Try to get from cache
-			cacheClient.get(droneId, (cacheErr, droneFromCache) =>
-			{
-				if (cacheErr) {
-					console.log('Cache error', cacheErr)
-				} else {
+	for (let tryNum = 1; tryNum <= maxTries; tryNum++)
+	{
+		// Try to get drone from origin
+		getDroneById(droneId,
+			originError => {
+				// Origin failed
+				console.log('Try', tryNum, 'Origin error', originError)
+				// Try to get from cache.  May have been fetched by another process.
+				cacheClient.get(droneId, (cacheErr, droneFromCache) => {
+					if (cacheErr) {
+						console.log('Try', tryNum, 'Cache error', cacheErr)
+						return // Try again
+					}
 					console.log('Got from cache', droneFromCache)
 					return done(droneFromCache)
-				}
+				})
+			},
+			droneFromOrigin => {
+				// Got drone from origin - cache and return it
+				cacheClient.set(droneId, droneFromOrigin, {expires: 600})
+					.catch(e => console.log('set failed', e))
+				return done(droneFromOrigin)
 			})
-		},
-		droneFromOrigin => {
-			// Got drone from origin - cache and return it
-			cacheClient.set(droneId, droneFromOrigin, {expires: 600})
-				.catch(e => console.log('set failed', e))
-			return done(droneFromOrigin)
-		})
+	}
 }
 
 module.exports = getCachedDroneById
